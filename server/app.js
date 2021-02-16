f347eb4676bad5e9f35e11a4635fc31efdd498dc
Jin Jin Heipler <heipler.jj@gmail.com>
2021-02-16 10:05:03 -0500

    Added main layout handlebars file

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.engine('handlebars', expressHandlebars({
-  defaultLayout: '',
+  defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
