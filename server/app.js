94270a924cc6d8f79852f9c620a6adf69f33efa5
julshotal <jgh6364@g.rit.edu>
2021-02-19 13:56:06 -0500

    Mongo and Redis setup

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const expressHandlebars = require('express-handlebars');
 
@@ -11,6 +12,30 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // pull in our routes
 const router = require('./router.js');
+
+//mongo bongo
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/Sprout';
+
+mongoose.connect(dbURL, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+//redis
+let redisURL = {
+  hostname: 'redis-16032.c245.us-east-1-3.ec2.cloud.redislabs.com',
+  port: 16032,
+};
+
+let redisPASS = 'sxQiggVY5xDl80DDHUX0KIjPE62XwQSJ';
+
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPASS = redisURL.auth.split(':')[1];
+}
+
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
