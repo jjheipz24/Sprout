a5e65ccd987a037b7b24fa7b97d1975b8126f9aa
Ella Mathewson <ellaofohio@gmail.com>
2021-02-25 09:21:19 -0500

    Added a root route code

--- server/router.js
+++ server/router.js
@@ -8,6 +8,13 @@ const router = (app) => {
   app.post('/login', controllers.Account.login);
   app.get('/signup', controllers.Account.signupPage);
   app.post('/signup', controllers.Account.signup);
+  
+  app.get('/', controllers.Account.loginPage);
+  /*
+    Figured this out with the help of https://stackoverflow.com/questions/6528876/how-to-redirect-404-errors-to-a-page-in-expressjs
+  */
+  // app.get('*', mid.requiresLogin, controllers.Account.errorPage);
+
 };
 
 module.exports = router;
