1d58be86ba5b7114fa23a0ca1e5f97cf18ae3271
Jin Jin Heipler <heipler.jj@gmail.com>
2021-03-10 04:10:00 -0500

    Began experimentation with functionality for planting certain plants and having them saved

--- hosted/pixi.js
+++ hosted/pixi.js
@@ -1,226 +1,132 @@
-// const { render } = require("node-sass");
-
 // checks to make sure document is loaded
 document.addEventListener('DOMContentLoaded', function () {
     let popup = document.getElementById("message");
 
+    let w = window.innerWidth;
+    let h = window.innerHeight;
     // Creates pixi app
-    const app = new PIXI.Application({
+    // const app = new PIXI.Application({
+    const app = new PIXI.Renderer({
         backgroundColor: 0x043535,
-        resizeTo: window,
-        view: document.querySelector('#pixiCanvas')
+        width: w,
+        height: h,
+        view: document.querySelector('#pixiCanvas'),
+        resolution: window.devicePixelRatio,
+        autoDensity: true
     });
 
-    //resizeTo(100, 100);
+    window.addEventListener('resize', resize);
 
-    document.body.appendChild(app.view);
+    function resize() {
+        w = window.innerWidth;
+        h = window.innerHeight;
 
+        app.resize(w, h);
+    }
 
     // Scale mode for all textures, will retain pixelation
     PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
-    // creates sprite from image
-    //const sprite = PIXI.Sprite.from('plant.png');
+    const container = new PIXI.Container(); //main container
 
-    const container = new PIXI.Container();
+    Storage.prototype.setObj = function (key, obj) {
+        return this.setItem(key, JSON.stringify(obj))
+    }
+    Storage.prototype.getObj = function (key) {
+        return JSON.parse(this.getItem(key))
+    }
 
-    // Move container to the center
-    container.x = (app.screen.width / 2);
-    container.y = app.screen.height / 2;
+    let circles = []; //Holds the white placeholder spots
+    let colors = {}; //holds the color textures
 
-    // Center sprite in local container coordinates
-    container.pivot.x = container.width / 2;
-    container.pivot.y = container.height / 2;
-    app.stage.addChild(container);
+    let white = PIXI.Texture.from('assets/images/test/white.png');
+
+    let blue = PIXI.Texture.from('assets/images/test/blue.png');
+    colors["blue"] = blue;
+    let orange = PIXI.Texture.from('assets/images/test/orange.png');
+    colors["orange"] = orange;
+    let green = PIXI.Texture.from('assets/images/test/green.png');
+    colors["green"] = green;
+    let pink = PIXI.Texture.from('assets/images/test/pink.png');
+    colors["pink"] = pink;
+    let purple = PIXI.Texture.from('assets/images/test/purple.png');
+    colors["purple"] = purple;
+    let yellow = PIXI.Texture.from('assets/images/test/yellow.png');
+    colors["yellow"] = yellow;
+
+    if (localStorage.getObj("circles") === null) {
+        initCircles();
+    } else {
+        circles = localStorage.getObj("circles");
+    }
+
+    // initCircles();
+    //Create the initial circles
+    function initCircles() {
+        for (let i = 0; i < 6; i++) {
+            let circle = new PIXI.Sprite(white);
+            circle.interactive = true;
+            circle.buttonMode = true;
+            circle.anchor.set(0.5);
+            circle.scale.set(.75, .75);
+            circle.x = (app.screen.width / 2) - ((i % 3) * 150) + 150;
+            circle.y = (app.screen.height / 2) - (Math.floor(i / 3) * 150);
+            circle.on('pointerdown', changeColor);
+
+            circles.push(circle);
+        }
+    }
+
+    circles.forEach(circle => {
+        container.addChild(circle);
+    })
 
 
+    //Handles changing the circle color
+    function changeColor() {
+        console.log(this.x);
+        let key = Object.keys(colors)[rand()];
+        this.texture = colors[key];
+    }
 
-    // Create grid of plants
-    // for (let i = 0; i < 5; i++) {
-    //     const plant = new PIXI.Sprite.from('assets/images/plant.png');
-    //     plant.anchor.set(0.5);
-    //     plant.x = (i % 5) * 80;
-    //     plant.y = Math.floor(i / 5) * 40;
-    //     plant.scale.set(.075, .075);
+    //Generates a random number based on number of textures
+    function rand() {
+        return Math.floor(Math.random() * Math.floor(Object.keys(colors).length));
+    }
 
-    //     //making plants interactive
-    //     plant.interactive = true;
-    //     plant.buttonMode = true;
-    //     plant.on('pointerdown', onClick);
+    // let test = new PIXI.Sprite(blue);
+    // let test2 = new PIXI.Sprite(orange);
+    // test.anchor.set(0.5);
+    // test.scale.set(.75, .75);
+    // test2.anchor.set(0.5);
+    // test2.scale.set(.75, .75);
 
-    //     container.addChild(plant);
-    // }
+    // colors.push(test2);
 
-    // Set the initial position
-    // sprite.anchor.set(0.5);
-    // sprite.x = app.screen.width / 2;
-    // sprite.y = app.screen.height / 2;  
+    // colors[0].x = 500;
+    // colors[0].y = 300;
 
+    // container.addChild(test);
+    // container.addChild(colors[0]);
 
 
-    let plants = []; //Holds an array of the plants spawned
-    let pos = 0; //sets the initial position
-    const staticButtonTexture = PIXI.Texture.from('assets/images/button.png');
-    const hoveredButtonTexture = PIXI.Texture.from('assets/images/button-hover.png');
+    /*********** Animation **********/
+    //Starts animation -- runs every few intervals
+    const ticker = new PIXI.Ticker();
+    ticker.add(animate);
+    ticker.start();
 
-    //BUTTON: Generates a new plant
-    const addPlantButton = new PIXI.Sprite(staticButtonTexture);
-    addPlantButton.anchor.set(0.5);
-    addPlantButton.interactive = true;
-    addPlantButton.buttonMode = true;
-    addPlantButton.x = 120;
-    addPlantButton.y = 100;
-
-    addPlantButton
-        .on('pointerdown', addPlant)
-        .on('pointerover', onButtonOver)
-        .on('pointerout', onButtonOut);
-    app.stage.addChild(addPlantButton);
-
-    //Adds plants
-    function addPlant() {
-
-        //Only lets 6 plants get planted
-        if (pos < 6) {
-            let newPlant = new PIXI.Sprite.from('assets/images/plant.png');
-            newPlant.anchor.set(0.5);
-            newPlant.x = (pos % 3) * 80;
-            newPlant.y = Math.floor(pos / 3) * 100;
-            newPlant.scale.set(.075, .075);
-
-            newPlant.interactive = true;
-            newPlant.buttonMode = true;
-            newPlant.on('pointerdown', onClick);
-            plants.push(newPlant);
-            container.addChild(newPlant);
-
-            pos++;
-        } else {
-            console.log("Too many plants");
+    //Place positioning in here to keep items positioned regardless of window size
+    function animate() {
+        // test.x = app.screen.width / 2;
+        // test.y = app.screen.height / 2;
+        for (let i = 0; i < 6; i++) {
+            circles[i].x = (app.screen.width / 2) - ((i % 3) * 150) + 150;
+            circles[i].y = (app.screen.height / 2) - (Math.floor(i / 3) * 150);
         }
 
-    }
+        // localStorage.setObj("circles", circles);
 
-    let numClicks = 0;
-
-    function onClick() {
-        if (numClicks < 5) {
-            this.scale.x *= 1.25;
-            this.scale.y *= 1.25;
-            numClicks++;
-            displayPopup();
-        } else {
-            console.log("cant grow any more today :(");
-        }
-
-    }
-
-    function displayPopup() {
-        popup.style.display = "block";
-    }
-
-    function onButtonOver() {
-        this.texture = hoveredButtonTexture;
-    }
-
-    function onButtonOut() {
-        this.texture = staticButtonTexture;
-    }
-
-    // Opt-in to interactivity
-    // sprite.interactive = true;
-
-    //   // scales sprite
-    //   sprite.scale.set(.1 , .1 );
-
-    // // Shows hand cursor
-    // sprite.buttonMode = true;
-
-    // // Pointers normalize touch and mouse
-    // sprite.on('pointerdown', onClick);
-
-    // // Alternatively, use the mouse & touch events:
-    // // sprite.on('click', onClick); // mouse-only
-    // // sprite.on('tap', onClick); // touch-only
-
-    // app.stage.addChild(sprite);
-
-    for (let i = 0; i < 5; i++) {
-        createSeed(
-            ((i % 5) * 40) + 50,
-            (Math.floor(i / 5) * 40) + 25,
-        );
-    }
-
-    function createSeed(x, y) {
-        // create seed
-        const plant = PIXI.Sprite.from('assets/images/seed.png');
-
-        // scales sprite
-        plant.scale.set(.1, .1);
-
-        // enable the seed to be interactive... this will allow it to respond to mouse and touch events
-        plant.interactive = true;
-
-        // this button mode will mean the hand cursor appears when you roll over the seed with your mouse
-        plant.buttonMode = true;
-
-        // center the seed's anchor point
-        plant.anchor.set(0.5);
-
-        // make it a bit bigger, so it's easier to grab
-        //plant.scale.set(3);
-
-        // setup events for mouse + touch using
-        // the pointer events
-        plant
-            .on('pointerdown', onDragStart)
-            .on('pointerup', onDragEnd)
-            .on('pointerupoutside', onDragEnd)
-            .on('pointermove', onDragMove)
-
-
-        // For mouse-only events
-        // .on('mousedown', onDragStart)
-        // .on('mouseup', onDragEnd)
-        // .on('mouseupoutside', onDragEnd)
-        // .on('mousemove', onDragMove);
-
-        // For touch-only events
-        // .on('touchstart', onDragStart)
-        // .on('touchend', onDragEnd)
-        // .on('touchendoutside', onDragEnd)
-        // .on('touchmove', onDragMove);
-
-        // move the sprite to its designated position
-        plant.x = x;
-        plant.y = y;
-
-        // add it to the stage
-        app.stage.addChild(plant);
-    }
-
-    function onDragStart(event) {
-        // store a reference to the data
-        // the reason for this is because of multitouch
-        // we want to track the movement of this particular touch
-        this.data = event.data;
-        this.alpha = 0.5;
-        this.dragging = true;
-    }
-
-    function onDragEnd() {
-        this.alpha = 1;
-        this.dragging = false;
-        // set the interaction data to null
-        this.data = null;
-    }
-
-    function onDragMove() {
-        if (this.dragging) {
-            const newPosition = this.data.getLocalPosition(this.parent);
-            this.x = newPosition.x;
-            this.y = newPosition.y;
-        }
+        app.render(container);
     }
 });
