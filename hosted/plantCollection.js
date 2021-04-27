b2105a7de3b7fc482fbd6cbca9934d999cc56656
julshotal <jgh6364@g.rit.edu>
2021-04-27 09:57:50 -0400

    spritesheet pog

--- hosted/plantCollection.js
+++ hosted/plantCollection.js
@@ -26,31 +26,31 @@ loader
 function init() {
     console.log("sprite sheets loaded!");
     const brownSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/brown.json"].spritesheet;
-    plotTex = brownSheet.animations["brown"]
+    plotTex = brownSheet.animations["brown"];
 
     let aloeSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/aloe.json"].spritesheet;
     let aloeSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/aloe-sprout.json"].spritesheet;
     plantCollection["aloe"] = [];
     plantCollection["aloe"][0] = aloeSproutSheet.animations["aloe-sprout"];
-    plantCollection["aloe"][1] = aloeSheet.animations["aloe"];
+    plantCollection["aloe"][1] = aloeSheet.animations["Aloe"];
 
     let fiddleSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-leaf.json"].spritesheet;
     let fiddleSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-sprout.json"].spritesheet;
     plantCollection["fiddle"] = [];
     plantCollection["fiddle"][0] = fiddleSproutSheet.animations["fiddle-sprout"];
-    plantCollection["fiddle"][1] = fiddleSheet.animations["fiddle-leaf"];
+    plantCollection["fiddle"][1] = fiddleSheet.animations["Fiddle Leaf"];
 
     let jadeSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade.json"].spritesheet;
     let jadeSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade-sprout.json"].spritesheet;
     plantCollection["jade"] = [];
     plantCollection["jade"][0] = jadeSproutSheet.animations["jade-sprout"];
-    plantCollection["jade"][1] = jadeSheet.animations["jade"];
+    plantCollection["jade"][1] = jadeSheet.animations["Jade"];
 
     let peaceSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily.json"].spritesheet;
     let peaceSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily-sprout.json"].spritesheet;
     plantCollection["peace"] = [];
     plantCollection["peace"][0] = peaceSproutSheet.animations["peace-lily-sprout"];
-    plantCollection["peace"][1] = peaceSheet.animations["peace-lily"];
+    plantCollection["peace"][1] = peaceSheet.animations["Peace Lilly"];
 
     let snakeSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake.json"].spritesheet;
     let snakeSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake-sprout.json"].spritesheet;
@@ -62,7 +62,7 @@ function init() {
     let cactusSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/cactus-sprout.json"].spritesheet;
     plantCollection["cactus"] = [];
     plantCollection["cactus"][0] = cactusSproutSheet.animations["cactus-sprout"];
-    plantCollection["cactus"][1] = cactusSheet.animations["cactus"];
+    plantCollection["cactus"][1] = cactusSheet.animations["Cactus"];
 
     /*************************************************************/
 }
