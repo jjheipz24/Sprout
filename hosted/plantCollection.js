ce2693d7c65a58f085cda37b302c9559bd53bae3
Jin Jin Heipler <heipler.jj@gmail.com>
2021-04-28 03:29:10 -0400

    all plants have seeds now so that's pretty poggers

--- hosted/plantCollection.js
+++ hosted/plantCollection.js
@@ -6,14 +6,19 @@ const loader = PIXI.Loader.shared;
 loader
     .add("assets/images/sprite-sheets/snake.json")
     .add("assets/images/sprite-sheets/snake-sprout.json")
+    .add("assets/images/sprite-sheets/snake-seeds.json")
     .add("assets/images/sprite-sheets/peace-lily.json")
     .add("assets/images/sprite-sheets/peace-lily-sprout.json")
+    .add("assets/images/sprite-sheets/peace-lily-seeds.json")
     .add("assets/images/sprite-sheets/jade.json")
     .add("assets/images/sprite-sheets/jade-sprout.json")
+    .add("assets/images/sprite-sheets/jade-seeds.json")
     .add("assets/images/sprite-sheets/fiddle-leaf.json")
     .add("assets/images/sprite-sheets/fiddle-sprout.json")
+    .add("assets/images/sprite-sheets/fiddle-seeds.json")
     .add("assets/images/sprite-sheets/cactus.json")
     .add("assets/images/sprite-sheets/cactus-sprout.json")
+    .add("assets/images/sprite-sheets/cactus-seeds.json")
     .add("assets/images/sprite-sheets/aloe.json")
     .add("assets/images/sprite-sheets/aloe-sprout.json")
     .add("assets/images/sprite-sheets/aloe-seeds.json")
@@ -40,32 +45,42 @@ function init() {
 
     let fiddleSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-leaf.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-leaf.json"].spritesheet : "no sheet";
     let fiddleSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-sprout.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-sprout.json"].spritesheet : "no sheet";
+    let fiddleSeedSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-seeds.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/fiddle-seeds.json"].spritesheet : "no sheet";
     plantCollection["fiddle"] = [];
-    plantCollection["fiddle"][0] = fiddleSproutSheet !== "no sheet" ? fiddleSproutSheet.animations["fiddle-sprout"] : [PIXI.Texture.from("assets/images/plants/fiddle-sprout.png")];
-    plantCollection["fiddle"][1] = fiddleSheet !== "no sheet" ? fiddleSheet.animations["Fiddle Leaf"] : [PIXI.Texture.from("assets/images/plants/fiddle-leaf.png")];
+    plantCollection["fiddle"][0] = fiddleSeedSheet !== "no sheet" ? fiddleSeedSheet.animations["fiddle-seeds"] : [PIXI.Texture.from("assets/images/plants/seeds.png")];
+    plantCollection["fiddle"][1] = fiddleSproutSheet !== "no sheet" ? fiddleSproutSheet.animations["fiddle-sprout"] : [PIXI.Texture.from("assets/images/plants/fiddle-sprout.png")];
+    plantCollection["fiddle"][2] = fiddleSheet !== "no sheet" ? fiddleSheet.animations["Fiddle Leaf"] : [PIXI.Texture.from("assets/images/plants/fiddle-leaf.png")];
 
     let jadeSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade.json"].spritesheet : "no sheet";
     let jadeSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade-sprout.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade-sprout.json"].spritesheet : "no sheet";
+    let jadeSeedSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade-seeds.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/jade-seeds.json"].spritesheet : "no sheet";
     plantCollection["jade"] = [];
-    plantCollection["jade"][0] = jadeSproutSheet !== "no sheet" ? jadeSproutSheet.animations["jade-sprout"] : [PIXI.Texture.from("assets/images/plants/jade-sprout.png")];
-    plantCollection["jade"][1] = jadeSheet !== "no sheet" ? jadeSheet.animations["Jade"] : [PIXI.Texture.from("assets/images/plants/jade.png")];
+    plantCollection["jade"][0] = jadeSeedSheet !== "no sheet" ? jadeSeedSheet.animations["jade-seeds"] : [PIXI.Texture.from("assets/images/plants/seeds.png")];
+    plantCollection["jade"][1] = jadeSproutSheet !== "no sheet" ? jadeSproutSheet.animations["jade-sprout"] : [PIXI.Texture.from("assets/images/plants/jade-sprout.png")];
+    plantCollection["jade"][2] = jadeSheet !== "no sheet" ? jadeSheet.animations["Jade"] : [PIXI.Texture.from("assets/images/plants/jade.png")];
 
     let peaceSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily.json"].spritesheet : "no sheet";
     let peaceSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily-sprout.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily-sprout.json"].spritesheet : "no sheet";
+    let peaceSeedSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily-seeds.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/peace-lily-seeds.json"].spritesheet : "no sheet";
     plantCollection["peace"] = [];
-    plantCollection["peace"][0] = peaceSproutSheet !== "no sheet" ? peaceSproutSheet.animations["peace-lily-sprout"] : [PIXI.Texture.from('assets/images/plants/peace-lily-sprout.png')];
-    plantCollection["peace"][1] = peaceSheet !== "no sheet" ? peaceSheet.animations["Peace Lilly"] : [PIXI.Texture.from('assets/images/plants/peace-lily.png')];
+    plantCollection["peace"][0] = peaceSeedSheet !== "no sheet" ? peaceSeedSheet.animations["peace-lily-seeds"] : [PIXI.Texture.from("assets/images/plants/seeds.png")];
+    plantCollection["peace"][1] = peaceSproutSheet !== "no sheet" ? peaceSproutSheet.animations["peace-lily-sprout"] : [PIXI.Texture.from('assets/images/plants/peace-lily-sprout.png')];
+    plantCollection["peace"][2] = peaceSheet !== "no sheet" ? peaceSheet.animations["Peace Lilly"] : [PIXI.Texture.from('assets/images/plants/peace-lily.png')];
 
     let snakeSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake.json"].spritesheet : "no sheet";
     let snakeSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake-sprout.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake-sprout.json"].spritesheet : "no sheet";
+    let snakeSeedSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake-seeds.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/snake-seeds.json"].spritesheet : "no sheet";
     plantCollection["snake"] = [];
-    plantCollection["snake"][0] = snakeSproutSheet !== "no sheet" ? snakeSproutSheet.animations["snake-sprout"] : [PIXI.Texture.from('assets/images/plants/snake-sprout.png')];
-    plantCollection["snake"][1] = snakeSheet !== "no sheet" ? snakeSheet.animations["snake-plant"] : [PIXI.Texture.from('assets/images/plants/snake.png')];
+    plantCollection["snake"][0] = snakeSeedSheet !== "no sheet" ? snakeSeedSheet.animations["snake-seeds"] : [PIXI.Texture.from("assets/images/plants/seeds.png")];
+    plantCollection["snake"][1] = snakeSproutSheet !== "no sheet" ? snakeSproutSheet.animations["snake-sprout"] : [PIXI.Texture.from('assets/images/plants/snake-sprout.png')];
+    plantCollection["snake"][2] = snakeSheet !== "no sheet" ? snakeSheet.animations["snake-plant"] : [PIXI.Texture.from('assets/images/plants/snake.png')];
 
     let cactusSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/cactus.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/cactus.json"].spritesheet : "no sheet";
     let cactusSproutSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/cactus-sprout.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/cactus-sprout.json"].spritesheet : "no sheet";
+    let cactusSeedSheet = PIXI.Loader.shared.resources["assets/images/sprite-sheets/cactus-seeds.json"] !== undefined ? PIXI.Loader.shared.resources["assets/images/sprite-sheets/cactus-seeds.json"].spritesheet : "no sheet";
     plantCollection["cactus"] = [];
-    plantCollection["cactus"][0] = cactusSproutSheet !== "no sheet" ? cactusSproutSheet.animations["cactus-sprout"] : [PIXI.Texture.from('assets/images/plants/cactus-sprout.png')];
-    plantCollection["cactus"][1] = cactusSheet !== "no sheet" ? cactusSheet.animations["Cactus"] : [PIXI.Texture.from('assets/images/plants/cactus.png')];
+    plantCollection["cactus"][0] = cactusSeedSheet !== "no sheet" ? cactusSeedSheet.animations["cactus-seeds"] : [PIXI.Texture.from("assets/images/plants/seeds.png")];
+    plantCollection["cactus"][1] = cactusSproutSheet !== "no sheet" ? cactusSproutSheet.animations["cactus-sprout"] : [PIXI.Texture.from('assets/images/plants/cactus-sprout.png')];
+    plantCollection["cactus"][2] = cactusSheet !== "no sheet" ? cactusSheet.animations["Cactus"] : [PIXI.Texture.from('assets/images/plants/cactus.png')];
     /*************************************************************/
 }
